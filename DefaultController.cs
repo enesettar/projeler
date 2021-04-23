65df16eb5359709a8f0b48b734a4ffa0310cde0f
enesettar <74353174+enesettar@users.noreply.github.com>
2021-04-23 17:04:03 +0300

    kişisel blog sitesi select denemesi eklendi

---
+++ DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DistinctBlog.Models.entity;
+using DistinctBlog.Models.sınıfım;
+
+namespace DistinctBlog.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        dbmvcblogyeniEntities db = new dbmvcblogyeniEntities();
+        public ActionResult Index()
+        {
+            Class1 cs = new Class1();
+            cs.deger1 = db.TBLHAKKIMDA.ToList();
+            cs.deger2 = db.TBLILETISIM.ToList();
+            
+            return View(cs);
+        }
+    }
+}
---
+++ sınıfım/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DistinctBlog.Models.entity;
+
+namespace DistinctBlog.Models.sınıfım
+{
+    public class Class1
+    {
+        public IEnumerable<TBLHAKKIMDA> deger1 { get; set; }
+        public IEnumerable<TBLILETISIM> deger2 { get; set; }
+    }
+}
